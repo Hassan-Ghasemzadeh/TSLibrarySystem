665e0f985967e440830c6439791f9da0f48db34e
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-06-27 11:31:54 +0330

    [Feat] : Filter and sort type are crreated

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -2,20 +2,20 @@ export type ID = string | number;
 export type Timestamp = Date;
 export type Status = "pending" | "active" | "inActive" | "deleted";
 
-export type BookId = string & { __brand: "BookId" }
-export type UserId = string & { __brand: "UserId" }
-export type TransactionId = string & { __brand: "TransactionId" }
+export type BookId = string & { __brand: "BookId" };
+export type UserId = string & { __brand: "UserId" };
+export type TransactionId = string & { __brand: "TransactionId" };
 
 export function createBrandedId<T extends string>(id: string): T {
     return id as T;
-}
+};
 
 export type ApiResponse<T> = {
     success: boolean,
     data?: T,
     error?: string,
     timestamp: Timestamp,
-}
+};
 
 export type PaginatedResult<T> = {
     items: T[];
@@ -23,4 +23,13 @@ export type PaginatedResult<T> = {
     page: number;
     pageSize: number;
     totalPages: number;
-}
+};
+
+export type FilterOptions<T> = {
+    [K in keyof T]?: T[K] | T[K][] | { min?: T[K], max?: T[K] };
+};
+ 
+export type SortOptions<T> = {
+    field: keyof T;
+    order: "asc" | "desc";
+};
