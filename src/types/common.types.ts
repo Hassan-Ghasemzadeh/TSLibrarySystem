92784956dd5e8d8878ba88c043b3a35e33bd78c3
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-06-27 11:19:04 +0330

    [Feat] : create helper function

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -5,3 +5,7 @@ export type Status = "pending" | "active" | "inActive" | "deleted";
 export type BookId = string & { __brand: "BookId" }
 export type UserId = string & { __brand: "UserId" }
 export type TransactionId = string & { __brand: "TransactionId" }
+
+export function createBrandedId<T extends string>(id: string): T {
+    return id as T;
+}
